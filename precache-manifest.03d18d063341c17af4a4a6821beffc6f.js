b2f39167ff39272d744f0e148a8719dbbfc230b0
Valer-Muresan <muresanvaler86@yahoo.com>
2020-01-28 16:46:00 +0200

    Updates

---
+++ precache-manifest.03d18d063341c17af4a4a6821beffc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd10374d052bebb7534e07ede31f8184",
+    "url": "/2020-Add-App-React/index.html"
+  },
+  {
+    "revision": "6dc8a98cc67d05eb2679",
+    "url": "/2020-Add-App-React/static/css/main.2d021b73.chunk.css"
+  },
+  {
+    "revision": "0dfc8b2574435a810253",
+    "url": "/2020-Add-App-React/static/js/2.43440a86.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/2020-Add-App-React/static/js/2.43440a86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dc8a98cc67d05eb2679",
+    "url": "/2020-Add-App-React/static/js/main.39d50804.chunk.js"
+  },
+  {
+    "revision": "7441d1a6d6632e908aae",
+    "url": "/2020-Add-App-React/static/js/runtime-main.bf6eca21.js"
+  }
+]);
